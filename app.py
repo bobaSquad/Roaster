0467ad28396b37488814d304f1aca46a7697a0ac
Adit-COCO-Garg <adit.garg21k@gmail.com>
2020-06-18 01:15:31 -0400

    Refactoring init code, added init cal intent

--- app.py
+++ app.py
@@ -1,46 +1,49 @@
 # import flask dependencies
 from flask import Flask, request, make_response, jsonify
-import intents
+# import intents
 
 # initialize the flask app
 app = Flask(__name__)
 
 
 # default route
-@app.route('/')
+@app.route('/', methods=['GET', "POST"])
 def index():
-    return 'DialogFlow server is running!'
+    req = request.get_json(silent=True)
+    return req if req is not None else {"Error": "JSON data not included in "
+                                                 "request"}
 
 
-# function for responses
-def results():
-    # build a request object
-    req = request.get_json(force=True)
-    print(req)
-
-    # fetch action from json
-    action = req.get('queryResult').get('action')
-    if action == "get-homework":
-        answer, longans = intents.gethomework()
-        if answer == "No":
-            return {'fulfillmentText': longans}
-        return {'fulfillmentText': longans}
-
-    if action == 'add-homework':
-        parameters = req.get('queryResult').get('parameters')
-        print(parameters)
-        ans = intents.addhomework(parameters)
-        return (ans)
-
-    # return a fulfillment response
-    return {'fulfillmentText': 'This is a response from webhook.'}
-
-
-# create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    # return response
     return make_response(jsonify(results()))
+
+
+def results():
+    # build a request object
+    req = request.get_json(silent=True)
+    # fetch intent from json
+    intent = req['queryResult']['intent']['displayName']
+    params = req['queryResult']['parameters']
+    # if intent == 'get-homework':
+    #     answer, longans = intents.gethomework()
+    #     if answer == 'No':
+    #         return {'fulfillmentText': longans}
+    #     return {'fulfillmentText': longans}
+    #
+    # if intent == 'add-homework':
+    #     parameters = req.get('queryResult').get('parameters')
+    #     print(parameters)
+    #     ans = intents.addhomework(parameters)
+    #     return (ans)
+
+    if intent == "init.cal":
+        email = params["email"]
+        return {'fulfillmentText': 'Calendar has been added! FFF'}
+
+    
+    # return a fulfillment response
+    return {'fulfillmentText': 'This is a response from webhook.'}
 
 
 # run the app
